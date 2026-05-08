fe62344149ffcd2e825099c287f7d0c1eb3daae9
ABHAY-st2580 <abhaysinghtomar835@gmail.com>
2026-05-09 00:34:59 +0530

    Frontend Starts..its journey

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+// import Navbar from "./components/Navbar/Navbar";
+// import AuthModal from "./components/Authcomp/Authcomp";
 
 function App() {
+  // const [showModal, setShowModal] = useState(false);
+
   return (
-    <>
-    </>
-  )
+    <div>
+      
+    </div>
+  );
 }
 
-export default App
+export default App;
