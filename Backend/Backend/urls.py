5440d51c04530d5be9ade0d7295b48c498a485dd
ABHAY-st2580 <abhaysinghtomar835@gmail.com>
2026-08-14 00:30:04 +0530

    Backend is ready for deployemnet

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.http import HttpResponse
 
 def home(request):
-    pass
+    return HttpResponse("Django backend is running!")
+    
 
 urlpatterns = [
     path('admin/', admin.site.urls),
